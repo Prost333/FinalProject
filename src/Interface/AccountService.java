6706b23023873568d3faa5e476aaf7c79999068e
prost333 <dimabolshoy333@yandex.by>
2023-05-26 16:07:09 +0300

    ty

--- src/Interface/AccountService.java
+++ src/Interface/AccountService.java
@@ -6,8 +6,5 @@ import javax.xml.crypto.Data;
 import java.util.List;
 
 public interface AccountService {
-    Account creatAccount (int userId,String accountNumer, String currency);
-    Account passMoney(int accountId, double money);
-    List<Account> getAccountByClient(int userId);
 
 }
